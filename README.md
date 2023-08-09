ed068d736f9e5e973f90513cb2b8cfe80331832d
claudiomenghi <cla.menghi@gmail.com>
2023-08-09 13:53:18 +0200

    Further cleans the project

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-This projects checks the consistency and completeness of Requirements Tables.
+This project provides a framework that enable the verification of the consistency and completeness of Requirements Tables.
 
-To run the project run 
-- RequirementsTable2Z3("ProjectExamples/MioProgetto.slx")
+The project is made by three parts:
+
+(i) The requirementExtractor: it extracts from a model the requirements associated with a given Requirement Tables. The requirementExtractor is a matlab function. It can be executed as follows:
+requirementsTable2requirements("Model", "DestinationFile");
+
+For example, the command
+
+requirementsTable2requirements("ProjectExamples/Example1.slx","ProjectExamples/Example1.txt");
+
+writes the requirements of the Requirements Tables contained in the Simulink model Example1 in the file requirementsExample1.txt
+
+(ii)
+
 
 The project is structured as follows 
 
--- requirementsTable2requirements.m
+++ requirementsTable2requirements.m
@@ -1,13 +1,15 @@
-function [] = RequirementsTable2Z3(modelpath)
+function [] = RequirementsTable2Z3(modelpath,destinationFile)
 %RequirementsTable2Z3 Converts the Requirements Tables contained in the
-%filw with path path into Z3
-%   modelpath: the path of the file to be considered. for Example, to run
+%file into a textual format within the file destinationFile
+% Inputs:
+%   modelpath the path of the file to be considered. for Example, to run
 %   the scripts of the file ''ProjectExamples/MioProgetto.slx'' run
 %   RequirementsTable2Z3("ProjectExamples/MioProgetto.slx")
     
     disp(strcat("opening the model: ", modelpath))
     % Open the project in simulink
     open(modelpath)
+    fid = fopen(destinationFile,'wt');
     
     % Looks up the requirements associated with the current Simulink model and returns the results in a table
     reqTables=slreq.modeling.find(gcs);
@@ -28,11 +30,18 @@ function [] = RequirementsTable2Z3(modelpath)
 
             requirement=rrow(requirementIterator);
             disp(strcat("Requirement: ",requirement.Index))
+            fprintf(fid, requirement.Index);
             disp(strcat("Precondition: ",rrow(requirementIterator).Preconditions))
+            fprintf(fid, rrow(requirementIterator).Preconditions{1});
             disp(strcat("Postcondition: ",rrow(requirementIterator).Postconditions))
+            %fprintf(fid, rrow(requirementIterator).Postconditions{1});
+            %fprintf(fid, '\n');
         end
     end
+    fclose(fid);
 end
+
+
 
 
 
