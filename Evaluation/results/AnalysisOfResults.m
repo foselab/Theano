4f71f96cfc714abc5026be61ec00c3d70dd41433
claudiomenghi <cla.menghi@gmail.com>
2024-01-30 16:25:47 +0100

    Fixes the display of the results for the completeness check

--- Evaluation/results/AnalysisOfResults.m
+++ Evaluation/results/AnalysisOfResults.m
@@ -4,17 +4,16 @@ close all;
 %filters=["UeArFs" "UeArVs" "UeUfFs" "UeUfVs" "BeArFs" "BeArVs" "BeUfFs" "BeUfVs"];
 filters=["BeUfFs" "BeUfVs" "BeArFs" "BeArVs" ];
 tables=["Table1" "Table2" "Table3" "Table4"];
-%checks=["consistency" "completeness"];
 
 val=input("Which results do you want to analyze? Type \n 1 - for completeness\n 2 - for consistency\n");
 
 if val==1
-    checks=["completeness"];
+    check="completeness";
     filePath='./results_completeness_bounded.csv';
     averageTablePath='./.results_completeness_bounded.csv';
 
 elseif val==2 
-    checks=["consistency"];
+    check="consistency";
     filePath='./boundedResults_consistency.csv';
     averageTablePath='./resultsAverage_consistency.csv';
 else
@@ -23,8 +22,7 @@ end
 
 Table = readtable(filePath); 
 
-
-checkStatistics(Table,checks,tables,filters);
+checkStatistics(Table,check,tables,filters);
 
 computingAverage=1;
 
@@ -35,73 +33,63 @@ end
 AverageTable = readtable(averageTablePath); 
 
 
+index=0;
 
-
-for check =checks
-    disp(check)
-
-   index=0;
-
-
-   for table = tables
-     disp(table)
-     index=index+1;
-     f = figure('Name',check);
-     orient landscape;
-     set(gcf, 'Color', 'white');
-     set(f, 'Color', 'white');
+for table = tables
+    index=index+1;
+    f = figure('Name',check);
+    orient landscape;
+    set(gcf, 'Color', 'white');
+    set(f, 'Color', 'white');
        
-        for filter = filters
-            disp(filter)        
-            plotter(AverageTable,filter,table,check,index)
-   
-        end
-        legend('Location', 'north', 'Orientation', 'horizontal');
-        legend(filters);
-        set(gca, 'FontSize', 20); % Set font size for axes labels
-        titleFontSize = 20; % Set font size for title
-        set(gcf, 'PaperUnits', 'normalized')
-        set(gcf, 'PaperPosition', [0 0 1 1])
-        saveas(f, strcat(check,table,'.pdf'));
-   end
-   
-
-   %xlabel(check);
+    for filter = filters
+        plotter(AverageTable,filter,table,check,index);
+    end
+    legend('Location', 'north', 'Orientation', 'horizontal');
+    legend(filters);
+    set(gca, 'FontSize', 20); % Set font size for axes labels
+    titleFontSize = 20; % Set font size for title
+    set(gcf, 'PaperUnits', 'normalized')
+    set(gcf, 'PaperPosition', [0 0 1 1])
+    saveas(f, strcat(check,table,'.pdf'));
 end
+
 
 disp('--------------------------')
 disp('Generic statistics')
 
 
-disp(strcat("*****Completeness ", check, "*****"));
-for filter = filters
-    rf=rowfilter(Table);
-    FilteredTable2=Table(rf.check==check & rf.encoding==filter & rf.result=="Complete" & rf.expectedResult=="Complete", :);
-    Time1=FilteredTable2(:,"time");
-    timeValues1=Time1{:,1};
+if val==1
+    disp(strcat("*****Completeness ", check, "*****"));
+    for filter = filters
+        rf=rowfilter(Table);
+        FilteredTable2=Table(rf.check==check & rf.encoding==filter & rf.result=="Complete" & rf.expectedResult=="Complete", :);
+        Time1=FilteredTable2(:,"time");
+        timeValues1=Time1{:,1};
+    
+        FilteredTable3=Table(rf.check==check & rf.encoding==filter & rf.result=="Incomplete" & rf.expectedResult=="Incomplete", :);
+        Time2=FilteredTable3(:,"time");
+        timeValues2=Time2{:,1};
+        timeValues=[timeValues1; timeValues2];
+        disp(strcat("check: ", check, "   encoding: ", filter, "       mean: ", num2str(mean(timeValues)), "      min: ", num2str(min(timeValues)), "      max: ", num2str(max(timeValues)),"      std: ", num2str(std(timeValues))));
+    end
+end
 
-    FilteredTable3=Table(rf.check==check & rf.encoding==filter & rf.result=="Incomplete" & rf.expectedResult=="Incomplete", :);
-    Time2=FilteredTable3(:,"time");
-    timeValues2=Time2{:,1};
-    timeValues=[timeValues1; timeValues2];
-    disp(strcat("check: ", check, "   encoding: ", filter, "       mean: ", num2str(mean(timeValues)), "      min: ", num2str(min(timeValues)), "      max: ", num2str(max(timeValues)),"      std: ", num2str(std(timeValues))));
+if val==2 
+    disp(strcat("*****Consistency ", check, "*****"));
+    for filter = filters
+        rf=rowfilter(Table);
+        FilteredTable3=Table(rf.check==check & rf.encoding==filter & rf.result=="Inconsistent" & rf.expectedResult=="Inconsistent", :);
+        Time2=FilteredTable3(:,"time");
+        timeValues2=Time2{:,1};
+        timeValues=timeValues2;
+        disp(strcat("check: ", check, "   encoding: ", filter, "       mean: ", num2str(mean(timeValues)), "      min: ", num2str(min(timeValues)), "      max: ", num2str(max(timeValues)),"      std: ", num2str(std(timeValues))));
+    end
 end
 
 
-disp(strcat("*****Consistency ", check, "*****"));
-for filter = filters
-    rf=rowfilter(Table);
-    FilteredTable3=Table(rf.check==check & rf.encoding==filter & rf.result=="Inconsistent" & rf.expectedResult=="Inconsistent", :);
-    Time2=FilteredTable3(:,"time");
-    timeValues2=Time2{:,1};
-    timeValues=timeValues2;
-    disp(strcat("check: ", check, "   encoding: ", filter, "       mean: ", num2str(mean(timeValues)), "      min: ", num2str(min(timeValues)), "      max: ", num2str(max(timeValues)),"      std: ", num2str(std(timeValues))));
-end
-
-
-function [] = checkStatistics(Table,checks,tables,filters)
-   for check =checks
-
+function [] = checkStatistics(Table,check,tables,filters)
+   
        for filter = filters
           filterfinalcount=0;
           filterfinalcorrect=0;
@@ -134,8 +122,7 @@ function [] = checkStatistics(Table,checks,tables,filters)
           end
           disp(strcat("check: ",check, "  encoding: ", filter, "  correct verdicts: ", num2str(filterfinalcorrect/filterfinalcount*100), '%'))
        end
-   end
-   disp(strcat("Total Time Min: ",num2str(sum(Table.time)/60)));
+      disp(strcat("Total Time Min: ",num2str(sum(Table.time)/60)));
 end
 
 function [] = computeAverageTable(filePath,averageTablePath)
--- Evaluation/results/plotter.m
+++ Evaluation/results/plotter.m
@@ -10,12 +10,13 @@ function [] = plotter(Table,filter,table,check,index)
     boundValues=Bound{:,1};
 
 
-    %subplot(2,2,index)
-    plot(boundValues,timeValues,'-o');
+    [sortBoundValues, sortidx] = sort(boundValues);
+    sortTimeValues = timeValues(sortidx);
+    plot(sortBoundValues,sortTimeValues,'-o');
     hold on;
     grid on;
 
-    disp(strcat("mean: ", num2str(mean(timeValues)),"      max: ", num2str(max(timeValues)), "      min: ", num2str(min(timeValues)), "      std: ", num2str(std(timeValues))));
+    %disp(strcat("mean: ", num2str(mean(timeValues)),"      max: ", num2str(max(timeValues)), "      min: ", num2str(min(timeValues)), "      std: ", num2str(std(timeValues))));
    % title(strcat(table,": ",FilteredTable{1,7}{1}))
     ylabel('Time [s]');
     xlabel('Threshold');
